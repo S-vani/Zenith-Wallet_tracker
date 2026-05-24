4eead96921b7a33126d2c496c9269a393698d8d7
S-vani <shayan.vanishoja86@gmail.com>
2026-05-24 14:34:47 -0400

    Added colouring to the dashboard page to show green for profit and red for loss

--- frontend/src/Dashboard/DashboardStats.jsx
+++ frontend/src/Dashboard/DashboardStats.jsx
@@ -1,4 +1,14 @@
 function DashboardStats({ stats }) {
+
+    let statName = "stat-value"
+    if (stats.curr_timeperiod > 0) {
+        statName += " positive"
+    }
+    else{
+        statName += " negative"
+    }
+
+
     return (
         <div className="dashboard-stats">
             <div className="stat-card">
@@ -8,7 +18,8 @@ function DashboardStats({ stats }) {
 
             <div className="stat-card">
                 <div className="stat-label">Period Return</div>
-                <div className="stat-value">$ {stats.curr_timeperiod.toFixed(2)}</div>
+                <div className={statName}>$ {stats.curr_timeperiod.toFixed(2)}</div>
+
             </div>
         </div>
     );
