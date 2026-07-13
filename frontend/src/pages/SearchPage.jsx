7091c19e858ca2fd6f83d8fb6d4d767fa0fe1a76
S-vani <shayan.vanishoja86@gmail.com>
2026-07-12 20:09:30 -0400

    Search bar submit now calls backend search routes to return the information.

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,20 +1,35 @@
 import "../css/Search.css"
 import {useState} from "react";
+import {searchCrypto, searchStock} from "../services/api.js";
 
 function SearchPage (){
     const [searchSymbol, setSearchSymbol] = useState({"symbol": ""});
+    const [searchResults, setSearchResults] = useState([])
 
 
     function searchBarChange(e){
         setSearchSymbol({
             "symbol": e,
         })
-        console.log(e)
+    }
+
+    async function searchBarSubmit(){
+        const data = await searchStock(searchSymbol.symbol)
+        setSearchResults(data)
     }
 
     return (
         <div className="search-page-wrapper">
-            <input onChange={(e) => searchBarChange(e.target.value)} className="search-bar" placeholder="Insert Symbol"/>
+            <button>Crypto</button>
+            <button>Stock</button>
+            <input
+                onChange={(e) => searchBarChange(e.target.value)}
+                className="search-bar"
+                placeholder="Insert Symbol"
+            />
+            <button className="search-submit-button" onClick={() => searchBarSubmit()}>Search</button>
+
+            <button onClick={() => console.log(searchResults)}>Hello</button>
         </div>
     )
 }
