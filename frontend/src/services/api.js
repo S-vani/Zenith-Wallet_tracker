60e381da99f92c76b505130ac21ab946b5aced68
S-vani <shayan.vanishoja86@gmail.com>
2026-08-24 12:03:52 -0400

    changes to the env name of the backend url since it must start with vite

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -223,6 +223,7 @@ export async function getUserCount() {
     if (!res.ok) {
         throw new Error("error fetching user information")
     }
+    console.log(BASE_URL)
 
     return res.json()
 }
